d1cc5df4efc23ecf49c65a70ca1789b5133ac1b2
Michael Tsibelman <tsibelman@gmail.com>
2017-05-19 19:55:47 +0300

    Add copyright

--- Gigya.Microdot.SharedLogic/Measurement/ConcurrentStopwatch.cs
+++ Gigya.Microdot.SharedLogic/Measurement/ConcurrentStopwatch.cs
@@ -1,3 +1,25 @@
+#region Copyright 
+// Copyright 2017 Gigya Inc.  All rights reserved.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License"); 
+// you may not use this file except in compliance with the License.  
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+#endregion
+
 using System;
 using System.Diagnostics;
 using System.Threading;
@@ -11,16 +33,16 @@ namespace Gigya.Microdot.SharedLogic.Measurement
     public class ConcurrentStopwatch: IMeasurement {
 
         /// <summary>The total number of measurements performed and in progress.</summary>
-        long NumMeasurements = 0;
+        long NumMeasurements;
 
         /// <summary>The number of measurements that are currently in progress.</summary>
-        long NumMeasurementsInProgress = 0;
+        long NumMeasurementsInProgress;
 
         /// <summary>The timestamp (from Stopwatch.GetTimestamp()) at which the last measurement was started or ended.</summary>
-        long LastTimestamp = 0;
+        long LastTimestamp;
 
         /// <summary>The total number of ticks accumulated as of the last measurement that was started or ended.</summary>
-        long ElapsedTicks = 0;
+        long ElapsedTicks;
 
 
         /// <summary>Starts measuring time. Can be called multiple times to perform multiple measurements. Try using the
