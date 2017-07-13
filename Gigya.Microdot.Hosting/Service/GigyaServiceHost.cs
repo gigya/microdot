64aa00531178c841e3ae578e364fe329bec6c548
Allon Guralnek <allon.gu@gigya-inc.com>
2017-07-13 17:21:59 +0300

    Added ProcessorAffinity service argument, allowing to specify which processors should be used by the service (and adjust Orleans thread count accordingly). Removed OrleansConfig.MaxActiveThreads. Added all ServiceArguments properties to config endpoint.

--- Gigya.Microdot.Hosting/Service/GigyaServiceHost.cs
+++ Gigya.Microdot.Hosting/Service/GigyaServiceHost.cs
@@ -73,6 +73,16 @@ namespace Gigya.Microdot.Hosting.Service
             Arguments = argumentsOverride ?? new ServiceArguments(Environment.GetCommandLineArgs().Skip(1).ToArray());
             CurrentApplicationInfo.Init(ServiceName, Arguments.InstanceName);
 
+            if (Arguments.ProcessorAffinity != null)
+            {
+                int processorAffinityMask = 0;
+
+                foreach (var cpuId in Arguments.ProcessorAffinity)
+                    processorAffinityMask |= 1 << cpuId;
+
+                Process.GetCurrentProcess().ProcessorAffinity = new IntPtr(processorAffinityMask);
+            }
+
             if (Arguments.ServiceStartupMode == ServiceStartupMode.WindowsService)
             {
                 Trace.WriteLine("Service starting as a Windows service...");
