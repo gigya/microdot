7588b9812eefbcc83ad3f08186611d2fe1977dee
David Bronshtein <david.br@gigya-inc.com>
2017-08-09 16:45:54 +0300

    increase version number
    Commit by David Bronshtein <david.br@gigya-inc.com>
    On branch: refs/heads/Add_method_parameter_types_to_schema_endpoint

--- Gigya.ServiceContract/Properties/AssemblyInfo.cs
+++ Gigya.ServiceContract/Properties/AssemblyInfo.cs
@@ -40,9 +40,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 
 
-[assembly: AssemblyInformationalVersion("2.4.10")]// if pre-release should be in the format of "2.4.11-pre01".
-[assembly: AssemblyVersion("2.4.10")]
-[assembly: AssemblyFileVersion("2.4.10")]
+[assembly: AssemblyInformationalVersion("2.4.11")]// if pre-release should be in the format of "2.4.11-pre01".
+[assembly: AssemblyVersion("2.4.11")]
+[assembly: AssemblyFileVersion("2.4.11")]
 
 [assembly: AssemblyDescription("")]
 
