4cd2e7b0fc7417dd6fb860deb71ae77c0c42442d
eran <eran.of@gigya-inc.com>
2017-08-14 10:52:17 +0300

    Add prot overridd to RequestContext. (#68)

--- Gigya.Microdot.ServiceDiscovery/HostManagement/OverriddenRemoteHost.cs
+++ Gigya.Microdot.ServiceDiscovery/HostManagement/OverriddenRemoteHost.cs
@@ -25,27 +25,42 @@ using Gigya.Common.Contracts.Exceptions;
 
 namespace Gigya.Microdot.ServiceDiscovery.HostManagement
 {
-    public class OverriddenRemoteHost : RemoteHost
+    public class OverriddenRemoteHost : IEndPointHandle
     {
         private string ServiceName { get; }
+        public string HostName { private set;get; }
+        public int? Port { private set; get; }
 
-        internal OverriddenRemoteHost(string serviceName, string hostName, RemoteHostPool remoteHostPool) : base(hostName,remoteHostPool,new object())
+
+        internal OverriddenRemoteHost(string serviceName, string hostName, int? port = null)
         {
             ServiceName = serviceName;
+            HostName = hostName;
+            Port = port;
+
         }
 
-        public override bool ReportFailure(Exception ex = null)
+
+
+        public bool ReportFailure(Exception ex = null)
         {
             throw new MissingHostException("Failed to reach an overridden remote host. Please make sure the " +
-                "overrides specified are reachable from all services that participate in the request. See inner " +
-                "exception for details and tags for information on which override caused this issue.",
+                                           "overrides specified are reachable from all services that participate in the request. See inner " +
+                                           "exception for details and tags for information on which override caused this issue.",
                 ex,
                 unencrypted: new Tags
                 {
                     { "overriddenServiceName", ServiceName },
-                    { "overriddenEndpoint", HostName },
-                    { "originalHostList", HostPool.GetAllHosts() }
+                    { "overriddenHostName", HostName },
+                    { "overriddenPort", Port?.ToString() }
+
                 });
         }
+
+        public void ReportSuccess()
+        {
+        }
+
+
     }
 }
