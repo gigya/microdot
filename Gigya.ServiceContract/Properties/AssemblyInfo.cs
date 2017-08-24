32e5c4c50945b59e9411c2b42860bf281de0ac5c
Michael Tsibelman <tsibelman@gmail.com>
2017-08-24 14:49:25 +0300

    Add flag UsingRequestObject to PublicEndpointAttribute (#74)

--- Gigya.ServiceContract/Properties/AssemblyInfo.cs
+++ Gigya.ServiceContract/Properties/AssemblyInfo.cs
@@ -40,9 +40,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 
 
-[assembly: AssemblyInformationalVersion("2.4.11")]// if pre-release should be in the format of "2.4.11-pre01".
-[assembly: AssemblyVersion("2.4.11")]
-[assembly: AssemblyFileVersion("2.4.11")]
+[assembly: AssemblyInformationalVersion("2.4.12")]// if pre-release should be in the format of "2.4.11-pre01".
+[assembly: AssemblyVersion("2.4.12")]
+[assembly: AssemblyFileVersion("2.4.12")]
 
 [assembly: AssemblyDescription("")]
 
