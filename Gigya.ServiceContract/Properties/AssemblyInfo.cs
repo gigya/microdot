7356285bdc4967b55667f0f7c058f2ea5d05d1aa
Eran Ofer <eran.of@gigya-inc.com>
2017-10-16 18:26:57 +0300

    Change AssemblyVersion

--- Gigya.ServiceContract/Properties/AssemblyInfo.cs
+++ Gigya.ServiceContract/Properties/AssemblyInfo.cs
@@ -40,9 +40,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 
 
-[assembly: AssemblyInformationalVersion("2.4.15")]// if pre-release should be in the format of "2.4.11-pre01".
-[assembly: AssemblyVersion("2.4.15")]
-[assembly: AssemblyFileVersion("2.4.15")]
+[assembly: AssemblyInformationalVersion("2.4.16-pre01")]// if pre-release should be in the format of "2.4.11-pre01".
+[assembly: AssemblyVersion("2.4.16")]
+[assembly: AssemblyFileVersion("2.4.16")]
 
 [assembly: AssemblyDescription("")]
 
