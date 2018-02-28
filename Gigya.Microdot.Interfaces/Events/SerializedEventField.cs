61ddead355fb8b7be64f99333a64617d2b68ab6d
Daniel Lamberger <daniel.lamberger@gigya-inc.com>
2018-02-28 11:23:21 +0200

    Event publisher now optionally provides list of serialized event fields.

---
+++ Gigya.Microdot.Interfaces/Events/SerializedEventField.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Gigya.Microdot.Interfaces.Events
+{
+
+    /// <summary>
+    /// Represents a single serialized field marked as <see cref="EventFieldAttribute"/> from an <see cref="IEvent"/>
+    /// object, or a sub-field of a collection. Beware of writing the <see cref="Value"/> if <see cref="ShouldEncrypt"/>
+    /// is true.
+    /// </summary>
+    public class SerializedEventField
+    {
+        public string Name;
+        public string Value;
+        public bool   ShouldEncrypt;
+    }
+}
