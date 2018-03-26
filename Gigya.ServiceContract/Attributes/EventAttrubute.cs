bec87cef08b8071440349bfa8397434246c14e97
anatoliy.ar <anatoliy.arkove@gigya-inc.com>
2018-03-26 16:04:20 +0300

    Remove LogField Duplication

--- Gigya.ServiceContract/Attributes/EventAttrubute.cs
+++ Gigya.ServiceContract/Attributes/EventAttrubute.cs
@@ -22,13 +22,4 @@ namespace Gigya.ServiceContract.Attributes
     public class NonSensitiveAttribute : Attribute{}
 
 
-    /// <summary>Mark the parameter as containing log field data
-    /// by providing this attribute the class will be dissects into properties.
-    /// </summary>
-    [AttributeUsage(AttributeTargets.Parameter)]
-    public class LogFieldsAttribute : Attribute{}
-
-
-
-
 }
