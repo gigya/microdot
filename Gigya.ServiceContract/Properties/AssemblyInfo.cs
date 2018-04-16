875628303a0559cc565fc88a57e3e64c207366be
anatoliy.ar <anatoliy.arkove@gigya-inc.com>
2018-04-16 11:06:37 +0300

    prelease version

--- Gigya.ServiceContract/Properties/AssemblyInfo.cs
+++ Gigya.ServiceContract/Properties/AssemblyInfo.cs
@@ -40,7 +40,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 
 
-[assembly: AssemblyInformationalVersion("2.5.4")]// if pre-release should be in the format of "2.4.11-pre01".
+[assembly: AssemblyInformationalVersion("2.5.4-pre01")]// if pre-release should be in the format of "2.4.11-pre01".
 [assembly: AssemblyVersion("2.5.4")]
 [assembly: AssemblyFileVersion("2.5.4")]
 
