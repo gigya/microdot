0c8f7ff40af25101c5eb924e7b2663c775b53156
David Bronshtein <david.br@gigya-inc.com>
2018-04-17 15:55:44 +0300

    ServiceSchema - change 'HashCode' to 'Hash'

--- Gigya.ServiceContract/Properties/AssemblyInfo.cs
+++ Gigya.ServiceContract/Properties/AssemblyInfo.cs
@@ -40,9 +40,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 
 
-[assembly: AssemblyInformationalVersion("2.5.4")]// if pre-release should be in the format of "2.4.11-pre01".
-[assembly: AssemblyVersion("2.5.4")]
-[assembly: AssemblyFileVersion("2.5.4")]
+[assembly: AssemblyInformationalVersion("2.5.5")]// if pre-release should be in the format of "2.4.11-pre01".
+[assembly: AssemblyVersion("2.5.5")]
+[assembly: AssemblyFileVersion("2.5.5")]
 
 
 [assembly: AssemblyDescription("")]
