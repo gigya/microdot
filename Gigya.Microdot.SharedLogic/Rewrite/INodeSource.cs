0059a3ebf7b0d1fdeb417263219908fdb03a6f42
Allon Guralnek <allon.guralnek@gmail.com>
2018-04-26 17:32:07 +0300

    Integration with ServiceProxy rewrite.

--- Gigya.Microdot.SharedLogic/Rewrite/INodeSource.cs
+++ Gigya.Microdot.SharedLogic/Rewrite/INodeSource.cs
@@ -7,7 +7,7 @@ namespace Gigya.Microdot.SharedLogic.Rewrite
     /// <summary>
     /// A source which supplies updated list of available nodes for discovery of a specific service and environment
     /// </summary>
-    public interface INodeSource: IDisposable
+    public interface INodeSource : IDisposable
     {
         /// <summary>
         /// Initialize this source (e.g. Consul source needs initialization to start monitoring Consul)
@@ -28,12 +28,12 @@ namespace Gigya.Microdot.SharedLogic.Rewrite
         INode[] GetNodes();
 
         /// <summary>
-        /// Returns true only if service was undeployed on the nodes source (e.g. Consul)
+        /// Returns true if the service was undeployed.
         /// </summary>
         bool WasUndeployed { get; }
 
         /// <summary>
-        /// Whether this source supports services which are deployed on multiple environemnts
+        /// Whether this source supports services which are deployed on multiple environments
         /// </summary>
         bool SupportsMultipleEnvironments { get; }
     }
