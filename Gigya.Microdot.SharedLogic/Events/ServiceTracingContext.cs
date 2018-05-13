f03507e1f330605940acbc5a63d619e106d6ea36
anatoliy.ar <anatoliy.arkove@gigya-inc.com>
2018-05-13 10:52:40 +0300

    Amendments after code review with Eran Ofer - Part 2

--- Gigya.Microdot.SharedLogic/Events/ServiceTracingContext.cs
+++ Gigya.Microdot.SharedLogic/Events/ServiceTracingContext.cs
@@ -1,4 +1,26 @@
-﻿using System;
+﻿#region Copyright 
+// Copyright 2017 Gigya Inc.  All rights reserved.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License"); 
+// you may not use this file except in compliance with the License.  
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+#endregion
+
+
 using System.Collections.Generic;
 using System.Threading;
 
@@ -15,12 +37,16 @@ namespace Gigya.Microdot.SharedLogic.Events
 
         protected override void Add(string key, object value)
         {
-            if (Context.Value == null)
-                Context.Value = new Dictionary<string, object>();
-            else
-                Context.Value = new Dictionary<string, object>(Context.Value);
+            Dictionary<string, object> cloneDictionary = null;
 
-            Context.Value[key] = value;
+            if (Context.Value == null)
+                cloneDictionary= new Dictionary<string, object>();
+            else
+                cloneDictionary = new Dictionary<string, object>(Context.Value);
+
+            cloneDictionary[key] = value;
+
+            Context.Value = cloneDictionary;
         }
 
         protected override T TryGetValue<T>(string key)
