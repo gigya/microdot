72a20620405c82d00eb1bb63def9571706ae7098
Anatoliy.Ar <anatoliy.arkove@gigya-inc.com>
2018-06-17 14:19:43 +0300

    Moving SensitivityAttributesValidator from Infra to Microdot

---
+++ Gigya.Microdot.Hosting/Validators/SensitivityAttributesValidator.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection;
+using Gigya.Common.Contracts.Exceptions;
+using Gigya.Microdot.Hosting.HttpService;
+using Gigya.ServiceContract.Attributes;
+
+namespace Gigya.Microdot.Hosting.Validators
+{
+    public class SensitivityAttributesValidator : IValidator
+    {
+        private readonly IServiceInterfaceMapper _serviceInterfaceMapper;
+
+
+        public SensitivityAttributesValidator(IServiceInterfaceMapper serviceInterfaceMapper)
+        {
+            _serviceInterfaceMapper = serviceInterfaceMapper;
+        }
+
+        public void Validate()
+        {
+            foreach (var serviceInterface in _serviceInterfaceMapper.ServiceInterfaceTypes)
+            {
+                foreach (var method in serviceInterface.GetMethods())
+                {
+                    if (method.GetCustomAttribute(typeof(SensitiveAttribute)) != null && method.GetCustomAttribute(typeof(NonSensitiveAttribute)) != null)
+                    {
+                        throw new ProgrammaticException($"[Sensitive] and [NonSensitive] can't both be applied on the same method ({method.Name}) on serviceInterface ({serviceInterface.Name})");
+                    }
+                    foreach (var parameter in method.GetParameters())
+                    {
+                        if (parameter.GetCustomAttribute(typeof(SensitiveAttribute)) != null && parameter.GetCustomAttribute(typeof(NonSensitiveAttribute)) != null)
+                        {
+                            throw new ProgrammaticException($"[Sensitive] and [NonSensitive] can't both be applied on the same parameter ({parameter.Name}) in method ({method.Name}) on serviceInterface ({serviceInterface.Name})");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
