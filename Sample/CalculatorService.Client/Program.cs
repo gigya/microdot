752ef6960cfc277310455b2633435141a526cb95
Yevgeny Kaplan <yevgeny.kaplan@gigya-inc.com>
2018-08-28 17:42:33 +0300

    Cleanup code

--- Sample/CalculatorService.Client/Program.cs
+++ Sample/CalculatorService.Client/Program.cs
@@ -41,83 +41,27 @@ namespace CalculatorService.Client
                 kernel.Load<MicrodotModule>();
                 kernel.Load<NLogModule>();
 
-                kernel.Get<Func<DiscoveryConfig>>()();
-                kernel.Get<Func<DiscoveryConfig>>()();
-                DiscoveryConfig dConfig = kernel.Get<DiscoveryConfig>();
+                kernel.Bind<ConfigCreatorTest>().ToSelf().InTransientScope();
+
                 ConfigCreatorTest configCreator = kernel.Get<ConfigCreatorTest>();
-                IConfigObject config = configCreator.GetDiscoveryConfig();
-                DiscoveryConfig dConfig2 = configCreator.GetDiscConfig();
-                bool dConfigsEqual = ReferenceEquals(dConfig, dConfig2);
-                ISourceBlock<CacheConfig> sourceBlockByFunc = configCreator.GetISourceBlockByFunc();
-                ISourceBlock<MyConfig> sourceBlockDirect = configCreator.GetISourceBlockDirect();
-                ISourceBlock<MyConfig> sourceBlockByFactory = configCreator.GetISourceBlockByFactory();
-                bool sourceBlocksAreEqual = ReferenceEquals(sourceBlockDirect, sourceBlockByFactory);
+
                 Console.WriteLine("Sleeping");
                 Thread.Sleep(5000);
                 Console.WriteLine("Starting test...");
 
-                //TestService(serviceCreator, 200000);
                 TestConfig(kernel, configCreator, 2000000);
 
                 Console.ReadLine();
 
-                //Console.WriteLine($"Sum: {sum}");
+                ICalculatorService calculatorService = kernel.Get<ICalculatorService>();
+                int sum = calculatorService.Add(2, 3).Result;
+                Console.WriteLine($"Sum: {sum}");
             }
             catch (Exception ex)
 
             {
                 Console.Error.WriteLine(ex);
             }
-        }
-
-        private static dynamic GetGenericFuncCompiledLambda(Type configType, ConfigObjectCreatorWrapper cocWrapper, string functionName)
-        {
-            MethodInfo func = typeof(ConfigObjectCreatorWrapper).GetMethod(functionName).MakeGenericMethod(configType);
-            Expression instance = Expression.Constant(cocWrapper);
-            Expression callMethod = Expression.Call(instance, func);
-            Type delegateType = typeof(Func<>).MakeGenericType(configType);
-            Type parentExpressionType = typeof(Func<>).MakeGenericType(delegateType);
-
-            dynamic lambda = Expression.Lambda(parentExpressionType, callMethod).Compile();
-
-            return lambda;
-        }
-
-        internal static bool IsConfigObject(Type service)
-        {
-            return service.IsClass && service.IsAbstract == false && typeof(IConfigObject).IsAssignableFrom(service);
-        }
-
-        internal static bool IsSourceBlock(Type service)
-        {
-            return
-                service.IsGenericType &&
-                service.GetGenericTypeDefinition() == typeof(ISourceBlock<>) &&
-                IsConfigObject(service.GetGenericArguments().Single());
-        }
-
-        private static ConfigObjectCreator GetCreator(IKernel kernel, Type type)
-        {
-            var getCreator = kernel.Get<Func<Type, ConfigObjectCreator>>();
-            var uninitializedCreator = getCreator(type);
-            uninitializedCreator.Init();
-            return uninitializedCreator;
-        }
-
-        private static void TestService(ServiceCrreatorTest serviceCreator, int calls)
-        {   
-            Console.WriteLine("Service test is started");
-            Stopwatch sw = new Stopwatch();
-            sw.Start();
-            for (int i = 0; i < calls; i++)
-            {
-                ICalculatorService calculatorService = serviceCreator.GetCalculatorService();
-                //int sum = calculatorService.Add(2, 3).Result;
-            }
-
-            sw.Stop();
-
-            Console.WriteLine($"Service test is finished with {calls} calls in {sw.Elapsed.TotalSeconds} seconds");
         }
 
         private static void TestConfig(StandardKernel kernel, ConfigCreatorTest configCreator, int calls)
