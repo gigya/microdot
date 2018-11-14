0ebcc4e4874e878ea9704d6ffd8996b0fa3d72c8
Alexander Chirlin <alexander.chirlin@gigya-inc.com>
2018-11-14 18:48:06 +0200

    #72489, fix version advance

--- SolutionVersion.cs
+++ SolutionVersion.cs
@@ -30,7 +30,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("1.11.34.0")]
 [assembly: AssemblyFileVersion("1.11.34.0")] 
-[assembly: AssemblyInformationalVersion("1.11.33.0")]
+[assembly: AssemblyInformationalVersion("1.11.34.0")]
 
 
 // Setting ComVisible to false makes the types in this assembly not visible 
