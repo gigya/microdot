47f019b4d14ef0872abdd028b9b150e8203da90a
eran.ofer <eran.of@gigya-inc.com>
2019-06-18 20:15:31 +0300

    performance of ServiceActivator, replace the reflection with  expression tree

--- Gigya.Microdot.Hosting/HttpService/AbstractServiceActivator.cs
+++ Gigya.Microdot.Hosting/HttpService/AbstractServiceActivator.cs
@@ -1,12 +1,13 @@
-#region Copyright 
+#region Copyright
+
 // Copyright 2017 Gigya Inc.  All rights reserved.
-// 
-// Licensed under the Apache License, Version 2.0 (the "License"); 
-// you may not use this file except in compliance with the License.  
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER AND CONTRIBUTORS "AS IS"
 // AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 // IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -18,34 +19,72 @@
 // CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
-#endregion
+
+#endregion Copyright
 
 using System;
+using System.Collections.Concurrent;
 using System.Diagnostics;
+using System.Linq;
+using System.Linq.Expressions;
 using System.Reflection;
 using System.Threading.Tasks;
-using Gigya.Common.Contracts;
 
 namespace Gigya.Microdot.Hosting.HttpService
 {
     public abstract class AbstractServiceActivator : IActivator
     {
+        private readonly ConcurrentDictionary<ServiceMethod, Func<object, object[], Task>> _fastInvoke = new ConcurrentDictionary<ServiceMethod, Func<object, object[], Task>>();
+        private readonly ConcurrentDictionary<ServiceMethod, Func<Task, object>> _fastGetter = new ConcurrentDictionary<ServiceMethod, Func<Task, object>>();
+
         public async Task<InvocationResult> Invoke(ServiceMethod serviceMethod, object[] argumentsWithDefaults)
         {
             var invokeTarget = GetInvokeTarget(serviceMethod);
 
-
-
             var sw = Stopwatch.StartNew();
-            var task = (Task)serviceMethod.ServiceInterfaceMethod.Invoke(invokeTarget, argumentsWithDefaults);
+            var invoker = _fastInvoke.GetOrAdd(serviceMethod, (serviceMethod1) => CreateStrongTypeDelegate(serviceMethod.ServiceInterfaceMethod));
+            var task = invoker(invokeTarget, argumentsWithDefaults);
             await task.ConfigureAwait(false);
             var executionTime = sw.Elapsed;
-            var result = task.GetType().GetProperty("Result").GetValue(task);
+
+            var getTaskResult = _fastGetter.GetOrAdd(serviceMethod, CreateGetterForGetResult(task));
+            var result = getTaskResult(task);
 
             return new InvocationResult { Result = result, ExecutionTime = executionTime };
         }
 
-       
+        public Func<object, object[], Task> CreateStrongTypeDelegate(MethodInfo methodInfo)
+        {
+            var methodParams = methodInfo.GetParameters();
+            var arrayParameter = Expression.Parameter(typeof(object[]), "array");
+
+            var arguments =
+                methodParams.Select((p, i) => Expression.Convert(
+                        Expression.ArrayAccess(arrayParameter, Expression.Constant(i)), p.ParameterType))
+                    .Cast<Expression>()
+                    .ToList();
+
+            var instanceParameter = Expression.Parameter(typeof(object), "controller");
+
+            var instanceExp = Expression.Convert(instanceParameter, methodInfo.DeclaringType);
+            var callExpression = Expression.Call(instanceExp, methodInfo, arguments);
+
+            var bodyExpression = Expression.Convert(callExpression, typeof(Task));
+
+            return Expression.Lambda<Func<object, object[], Task>>(
+                    bodyExpression, instanceParameter, arrayParameter)
+                .Compile();
+        }
+
+        public Func<object, object> CreateGetterForGetResult(object task)
+        {
+            var inputTaskParam = Expression.Parameter(typeof(object), "inputTask");
+            var convertToStrongTypeTask = Expression.ConvertChecked(inputTaskParam, task.GetType());
+            var property = Expression.Property(convertToStrongTypeTask, nameof(Task<Object>.Result));
+            var returnWeekType = Expression.Convert(property, typeof(object));
+            var lambda = Expression.Lambda<Func<object, object>>(returnWeekType, inputTaskParam).Compile();
+            return lambda;
+        }
 
         protected abstract object GetInvokeTarget(ServiceMethod serviceMethod);
     }
