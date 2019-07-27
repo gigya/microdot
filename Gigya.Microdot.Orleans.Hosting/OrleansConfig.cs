84c838d492edc814bf8288efdf4b6d55475e59f9
Alex Chirlin <alexander.chirlin@sap.com>
2019-07-27 18:54:05 +0300

    95878, GrainAgeLimitInMins set validation of min value of 1.001 minute

--- Gigya.Microdot.Orleans.Hosting/OrleansConfig.cs
+++ Gigya.Microdot.Orleans.Hosting/OrleansConfig.cs
@@ -1,6 +1,7 @@
 ﻿using Gigya.Microdot.Interfaces.Configuration;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Gigya.Microdot.Orleans.Hosting
 {
@@ -34,7 +35,19 @@ namespace Gigya.Microdot.Orleans.Hosting
 
     public class GrainAgeLimitConfig
     {
+        /// <remarks>
+        /// The CollectionAgeLimit must be greater than CollectionQuantum, which is set to 00:01:00 (by default).
+        /// https://dotnet.github.io/orleans/Documentation/clusters_and_clients/configuration_guide/activation_garbage_collection.html
+        /// See CollectionAgeLimitValidator.cs details.
+        /// </remarks>
+        [Range(1.001d, double.MaxValue, ErrorMessage = "The GrainAgeLimitInMins " +
+                                                       "(CollectionAgeLimit) must be greater than CollectionQuantum, " +
+                                                       "which is set to 1 min (by default). The type is double.")]
         public double GrainAgeLimitInMins { get; set; }
+
+        /// <summary>
+        /// The full qualified type name to apply grain age limit.
+        /// </summary>
         public string GrainType { get; set; }
     }
 
