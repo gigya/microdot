6de67e0566b82c4dcab6e1f08fa4b69429947831
Yaron Thurm <yaron@gigya-inc.com>
2019-09-10 13:13:41 +0300

    Moved to netstandard2.0: Gigya.Microdot.ServiceProxy and Gigya.Microdot.ServiceDiscovery

--- Gigya.Microdot.ServiceProxy/Properties/AssemblyInfo.cs
+++ Gigya.Microdot.ServiceProxy/Properties/AssemblyInfo.cs
@@ -27,8 +27,6 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Gigya.Microdot.ServiceProxy")]
-[assembly: AssemblyProduct("Gigya.Microdot.ServiceProxy")]
 [assembly: InternalsVisibleTo("Gigya.Common.OrleansInfra.TestingTools")]
 [assembly: InternalsVisibleTo("Gigya.Common.Application.UnitTests")]
 [assembly: InternalsVisibleTo("Gigya.Microdot.Testing")]
