2596d97fb5cf6a92dfa7a1a45f8435f4b9197d80
Yaron Thurm <yaron@gigya-inc.com>
2019-09-10 15:03:57 +0300

    Removed delay in CalculatorService.Client

--- Sample/CalculatorService.Client/Program.cs
+++ Sample/CalculatorService.Client/Program.cs
@@ -12,7 +12,6 @@ namespace CalculatorService.Client
         {
             try
             {
-                System.Threading.Thread.Sleep(7000);
                 Environment.SetEnvironmentVariable("GIGYA_CONFIG_ROOT", Environment.CurrentDirectory);
                 Environment.SetEnvironmentVariable("GIGYA_CONFIG_PATHS_FILE", "");
                 Environment.SetEnvironmentVariable("GIGYA_ENVVARS_FILE", Environment.CurrentDirectory);
