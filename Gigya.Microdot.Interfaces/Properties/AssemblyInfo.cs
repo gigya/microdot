eda2228e2d75f2771df37de8fd2b8451fb80d5fa
Yaron Thurm <yaron@gigya-inc.com>
2019-09-10 14:25:29 +0300

    Moved to netcoreapp2.0/netstandard2.0 (all remaining projects except test projects)

--- Gigya.Microdot.Interfaces/Properties/AssemblyInfo.cs
+++ Gigya.Microdot.Interfaces/Properties/AssemblyInfo.cs
@@ -26,8 +26,6 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Gigya.Microdot.Interfaces")]
-[assembly: AssemblyProduct("Gigya.Microdot.Interfaces")]
 
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
--- Gigya.Microdot.Orleans.Ninject.Host/Properties/AssemblyInfo.cs
+++ Gigya.Microdot.Orleans.Ninject.Host/Properties/AssemblyInfo.cs
@@ -26,8 +26,6 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Gigya.Microdot.Orleans.Ninject.Host")]
-[assembly: AssemblyProduct("Gigya.Microdot.Orleans.Ninject.Host")]
 
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
