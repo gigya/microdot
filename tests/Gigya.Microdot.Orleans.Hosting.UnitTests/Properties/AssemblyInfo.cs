e24bc8fb760697156a5255a521c5a84c02898e83
Yaron Thurm <yaron@gigya-inc.com>
2019-09-11 10:05:20 +0300

    Moved remaining tests projects to netcoreapp2.0

--- tests/Gigya.Microdot.Orleans.Hosting.UnitTests/Properties/AssemblyInfo.cs
+++ tests/Gigya.Microdot.Orleans.Hosting.UnitTests/Properties/AssemblyInfo.cs
@@ -26,11 +26,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Gigya.Microdot.Orleans.Hosting.FunctionalTests")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Gigya.Microdot.Orleans.Hosting.FunctionalTests")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -53,5 +49,3 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
