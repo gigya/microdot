5d10fd68828d3122959b35275ee1fb8a34be3f3d
Yonatan Doron <yonatan.doron@sap.com>
2020-03-25 14:15:43 +0200

    HTTPS is now disabled by default (as requested by Daniel)

--- Gigya.Microdot.ServiceDiscovery/Config/DiscoveryConfig.cs
+++ Gigya.Microdot.ServiceDiscovery/Config/DiscoveryConfig.cs
@@ -74,7 +74,7 @@ namespace Gigya.Microdot.ServiceDiscovery.Config
         [Obsolete("To be deleted after discovery refactoring")]
         public double DelayMultiplier { get; set; } = 2;
 
-        public bool UseHttpsOverride { get; set; } = true;
+        public bool UseHttpsOverride { get; set; } = false;
 
         /// <summary>
         /// The discovery mode to use, e.g. whether to use DNS resolving, Consul, etc.
