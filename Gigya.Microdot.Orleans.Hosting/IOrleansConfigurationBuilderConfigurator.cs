4ffbb024a117c201dfcce10d8fbd99688c74b569
tal.wei <tal.weiss01@sap.com>
2021-06-01 17:00:52 +0300

    us/141100 adding a hoof to configure SiloHostBuilder

---
+++ Gigya.Microdot.Orleans.Hosting/IOrleansConfigurationBuilderConfigurator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Orleans.Hosting;
+
+namespace Gigya.Microdot.Orleans.Hosting
+{
+    /// <summary>
+    /// This interface is intended to act as a hook for configuring Orleans host builder 
+    /// </summary>
+    public interface IOrleansConfigurationBuilderConfigurator
+    {
+        /// <summary>
+        /// Configure the host builder before it is initialized by microdot
+        /// </summary>
+        /// <param name="siloHostBuilder"></param>
+        void PreInitializationConfiguration(SiloHostBuilder siloHostBuilder);
+
+        /// <summary>
+        /// Configure the host builder after it is initialized by microdot
+        /// </summary>
+        /// <param name="siloHostBuilder"></param>
+        void PostInitializationConfiguration(SiloHostBuilder siloHostBuilder);
+    }
+}
