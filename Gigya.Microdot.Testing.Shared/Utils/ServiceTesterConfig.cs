281b82803551e439c450d3dfd3194f3f13b2e4cb
Guy Rozenblat <guy.rozenblat@sap.com>
2022-04-26 09:57:17 +0300

    #167609 Addition of servicetester config

---
+++ Gigya.Microdot.Testing.Shared/Utils/ServiceTesterConfig.cs
@@ -0,0 +1,16 @@
+﻿using Gigya.Microdot.Interfaces.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gigya.Microdot.Testing.Shared.Utils
+{
+    [Serializable]
+    [ConfigurationRoot("ServiceTester", RootStrategy.ReplaceClassNameWithPath)]
+    public class ServiceTesterConfig : IConfigObject
+    {
+        public bool ShouldHandleHttpsConnection { get; set; } = true;
+
+        public string HttpsConnectionCertHash { get; set; } = "33ccad5538d93c2af131a3281ba5539ae80e0483";
+    }
+}
