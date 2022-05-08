7494731b1bf74124f640d5e7ad503a8d38cffe76
samis <sami.saul@sap.com>
2022-05-08 15:07:56 +0300

    hash / config / url

---
+++ Gigya.Microdot.Hosting/HttpService/Endpoints/ConfigurationResponseBuilderConfig.cs
@@ -0,0 +1,31 @@
+﻿using Gigya.Microdot.Interfaces.Configuration;
+using Newtonsoft.Json;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace Gigya.Microdot.Hosting.HttpService.Endpoints
+{
+    [ConfigurationRoot("ConfigurationResponseBuilder", RootStrategy.ReplaceClassNameWithPath)]
+    public class ConfigurationResponseBuilderConfig : IConfigObject
+    {
+        [JsonProperty]
+        private string EnvsToPublish { get; set; } = "DC, ZONE, REGION, ENV, CONSUL, OS, HOSTIPADDRESS";
+
+        [JsonIgnore]
+        public List<string> envs { get; private set; } 
+
+        [OnDeserialized]
+        private void OnDeserialized(StreamingContext context)
+        {
+            envs = EnvsToPublish?
+                .Split(new[] { '\n', '\r', '\t', ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(_ => _.Trim())
+                .Where(_ => !string.IsNullOrEmpty(_))
+                .OrderBy(_ => _)
+                .ToList()
+                ?? new List<string>();
+        }
+    }
+}
