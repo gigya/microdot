2ad6d5bc40890bcd7eb18851169bb7f4a4e13dc3
samis <sami.saul@sap.com>
2022-06-07 10:32:04 +0300

    orleans version and logs

--- Gigya.Microdot.Orleans.Ninject.Host/NinjectOrleansBinding/MicrodotServiceProviderWithScope.cs
+++ Gigya.Microdot.Orleans.Ninject.Host/NinjectOrleansBinding/MicrodotServiceProviderWithScope.cs
@@ -24,6 +24,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Ninject;
 using Ninject.Syntax;
 using System;
+using System.Threading;
 
 namespace Gigya.Microdot.Orleans.Ninject.Host.NinjectOrleansBinding
 {
@@ -37,7 +38,7 @@ namespace Gigya.Microdot.Orleans.Ninject.Host.NinjectOrleansBinding
         private readonly IResolutionRoot _resolver;
         internal readonly MicrodotNinjectScopeParameter _microdotNinectScopParameter;
         private readonly ScopeCache _cacheItem;
-        private readonly static object _obj = new object();
+        private readonly static object _obj = new();
 
 
         public MicrodotServiceProviderWithScope(IResolutionRoot resolver)
@@ -58,17 +59,35 @@ namespace Gigya.Microdot.Orleans.Ninject.Host.NinjectOrleansBinding
         {
             var guid = Guid.NewGuid().ToString("N");
             var fullName = type.FullName;
-            Console.WriteLine($"*** MicrodotServiceProviderWithScope - {fullName} - {guid}");
+            var lf = Environment.NewLine;
+            var id = $"on MicrodotServiceProviderWithScope - {fullName} - {guid}";
+            object res = null;
 
-            object res;
+            Console.WriteLine($"*** GetAwaiter {id}");
 
-            lock (_obj)
+            System.Threading.Tasks.Task.Delay(200).GetAwaiter().GetResult();
+
+            Console.WriteLine($"*** BeforeMonitor {id}");
+
+            Monitor.Enter(_obj);
+            try
             {
-                Console.WriteLine($"*** Inside1 lock on MicrodotServiceProviderWithScope - {fullName} - {guid}");
+                Console.WriteLine($"*** Inside1 {id}");
                 res = _resolver.Get(type, _microdotNinectScopParameter);
-                Console.WriteLine($"*** Inside2 lock on MicrodotServiceProviderWithScope - {fullName} - {guid}");
+                Console.WriteLine($"*** Inside2 {id} - {res == null}");
             }
-            Console.WriteLine($"*** After lock on MicrodotServiceProviderWithScope - {guid}");
+            catch (Exception ex)
+            {
+                Console.WriteLine($"*** Catch {id}");
+                Console.WriteLine($"*** Exception {id}{lf}{ex}");
+            }
+            finally
+            {
+                Console.WriteLine($"*** Finally {id}");
+                Monitor.Exit(_obj);
+            }
+
+            Console.WriteLine($"*** AfterMonitor {id}");
             return res;
         }
 
